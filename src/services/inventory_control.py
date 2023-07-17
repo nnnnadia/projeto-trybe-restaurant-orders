2d90d66baddad95c49b5d3abc5eefbc14ead5812
nnnnadia <tristaonadia@gmail.com>
2023-07-17 12:52:14 -0300

    Req 5: `check_recipe_availability()` and `consume_recipe()`

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -1,11 +1,9 @@
 from csv import DictReader
 from typing import Dict
-
 from src.models.dish import Recipe
 from src.models.ingredient import Ingredient
 
 BASE_INVENTORY = "data/inventory_base_data.csv"
-
 Inventory = Dict[Ingredient, int]
 
 
@@ -27,8 +25,18 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        for ingredient, quantity in recipe.items():
+            if (
+                ingredient not in self.inventory
+                or self.inventory[ingredient] < quantity
+            ):
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        if not self.check_recipe_availability(recipe):
+            raise ValueError("Sorry! Recipe not available")
+
+        for ingredient, quantity in recipe.items():
+            self.inventory[ingredient] -= quantity
