d89c258d77e3a323df9e936bf809949913efab2e
nnnnadia <tristaonadia@gmail.com>
2023-07-17 12:46:24 -0300

    Req 3: `__init__()` and `_load_menu_data()`

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,34 @@
+import csv
+from typing import Set
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes: Set[Dish] = set()
+        self.ingredients = set()
+        self._load_menu_data(source_path)
+
+    def _load_menu_data(self, source_path: str) -> None:
+        with open(source_path, newline="") as file:
+            recipe = csv.reader(file)
+            next(recipe)
+            for row in recipe:
+                dish_name = row[0]
+                price = float(row[1])
+                ingredient_name = row[2]
+                quantity = int(row[3])
+                ingredient = Ingredient(ingredient_name)
+                self.ingredients.add(ingredient)
+
+                dish = next(
+                    (dish for dish in self.dishes if dish.name == dish_name),
+                    None,
+                )
+                if dish is None:
+                    dish = Dish(dish_name, price)
+                    self.dishes.add(dish)
+
+                dish.add_ingredient_dependency(ingredient, quantity)
