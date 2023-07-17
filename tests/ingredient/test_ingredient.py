ae861af2adf304ae61b725e17a1ad23117bb2310
nnnnadia <tristaonadia@gmail.com>
2023-07-17 12:44:35 -0300

    Req 1: `test_ingredient()`

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,38 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("queijo mussarela")
+    ingredient_2 = Ingredient("queijo mussarela")
+    assert hash(ingredient_1) == hash(ingredient_2)
+
+    ingredient_1 = Ingredient("presunto")
+    ingredient_2 = Ingredient("presunto")
+    assert ingredient_1 == ingredient_2
+
+    ingredient_1 = Ingredient("presunto")
+    ingredient_2 = Ingredient("queijo mussarela")
+    assert hash(ingredient_1) != hash(ingredient_2)
+
+    ingredient_1 = Ingredient("presunto")
+    ingredient_2 = Ingredient("queijo mussarela")
+    assert ingredient_1 != ingredient_2
+
+    ingredient_1 = Ingredient("presunto")
+    assert repr(ingredient_1) == "Ingredient('presunto')"
+    assert ingredient_1.name == "presunto"
+
+    restrictions_expected = {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+    ingredient_1 = Ingredient("presunto")
+    ingredient_2 = Ingredient("queijo mussarela")
+
+    assert ingredient_1.restrictions != restrictions_expected
+    assert ingredient_2.restrictions == restrictions_expected
+
+    ingredient_1 = Ingredient("farinha")
+    restrictions_expected = {Restriction.GLUTEN}
+    assert set(ingredient_1.restrictions) == restrictions_expected
